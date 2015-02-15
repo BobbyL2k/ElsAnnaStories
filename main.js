54948bebf68be1985e1d8704272c71e7323309be
BobbyL2k <bobby_nh@live.com>
2015-02-15 14:47:16 +0700

    JSON DB     Add database support for “Multi-Shot” stories     Extend Elsa’s Power to "No","Different","Yes","Powerful" from “No”,”Yes”     Change key naming convention     Make “Words” sortable     Make “Setting” sort useful HTML     Add jQuery ;(     Add sorting capabilities     Change id naming convention JS     Move String.prototype.format to top     Change JSON management(to support the extended JSON and Sorting)     Open links in new tab     Add Sorting     Coloring sorted column

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
+// First, checks if it isn't implemented yet.
+if (!String.prototype.format) {
+	String.prototype.format = function() {
+		var args = arguments;
+		return this.replace(/{(\d+)}/g, function(match, number) { 
+			return typeof args[number] != 'undefined'
+			? args[number]
+			: match
+			;
+		});
+	};
+}
+
+var DataObj,IndexObj;
+
 var xmlhttp;
 if (window.XMLHttpRequest){
     xmlhttp=new XMLHttpRequest();
@@ -7,34 +22,33 @@ else{// for IE6, IE5 ***NOT TESTED***
 }
 xmlhttp.onreadystatechange=function(){
     if (xmlhttp.readyState==4 && xmlhttp.status==200){
-        loadTableData(xmlhttp.responseText);
+		var obj = JSON.parse(xmlhttp.responseText);
+		DataObj = obj['data'];
+		IndexObj = obj['index'];
+        loadTableData(DataObj);
     }
 }
 xmlhttp.open("GET","data.json",true);
 xmlhttp.send();
 
 function loadTableData(JsonData){
-	var obj = JSON.parse(JsonData);
 	var result = "";
-	for(var c=0; c<obj['data'].length; c++){
-		var objc = obj['data'][c];
-		// First, checks if it isn't implemented yet.
-		if (!String.prototype.format) {
-			String.prototype.format = function() {
-				var args = arguments;
-				return this.replace(/{(\d+)}/g, function(match, number) { 
-					return typeof args[number] != 'undefined'
-					? args[number]
-					: match
-					;
-				});
-			};
-		}
+	for(var c=0; c<JsonData.length; c++){
+		var objc = JsonData[c];
 		function getStoryData(index){
-			if(index in obj['index']['enum']){
-				return obj['index']['enum'][index][objc[index]];
+			var prefix='',postfix='';
+			if(index in IndexObj['prefix'])
+				prefix = IndexObj['prefix'][index];
+			if(index in IndexObj['postfix'])
+				postfix = IndexObj['postfix'][index];
+			if(index in IndexObj['enum']){
+				if(typeof IndexObj['enum'][index][objc[index]] == 'undefined')
+					return 'error';
+				return prefix+IndexObj['enum'][index][objc[index]]+postfix;
 			}
-			return objc[index];
+			if(index in objc)
+				return prefix+objc[index]+postfix;
+			return 'n/a';
 		}
 		result += '<tr><td>{0}</td><td>'.format(
 				getStoryData('Title')
@@ -43,17 +57,17 @@ function loadTableData(JsonData){
 			result+= '<div class="hshow"><div>i</div><div class="textBlock">{0}</div></div>'.format(getStoryData('Detail'));
 		}
 		if('FF' in objc){
-			result+= '<div class=\"hshow\"><div><a href=\"{0}\">F</a></div><div>FanFiction.net</div></div>'.format(getStoryData('FF'));
+			result+= '<div class=\"hshow\"><div><a href=\"{0}\" target="_blank">F</a></div><div>FanFiction.net</div></div>'.format(getStoryData('FF'));
 		}
 		if('AO3' in objc){
-			result+= '<div class=\"hshow\"><div><a href=\"{0}\">A</a></div><div>ArchiveOfOurOwn.org</div></div>'.format(getStoryData('AO3'));
+			result+= '<div class=\"hshow\"><div><a href=\"{0}\" target="_blank">A</a></div><div>ArchiveOfOurOwn.org</div></div>'.format(getStoryData('AO3'));
 		}
 		if('Other' in objc){
-			result+= '<div class=\"hshow\"><div><a href=\"{0}\">O</a></div><div>Other</div></div>'.format(getStoryData('Other'));
+			result+= '<div class=\"hshow\"><div><a href=\"{0}\" target="_blank">O</a></div><div>Other</div></div>'.format(getStoryData('Other'));
 		}
 		result += '</td><td>{0}</td><td>{1}</td><td>{2}</td><td>{3}</td><td>{4}</td><td>{5}</td><td>{6}</td><td>{7}</td><td>{8}</td><td>{9}</td><td>{10}</td><td>{11}</td><td>{12}</td></tr>'.format(
 				getStoryData('Author'),
-				getStoryData('Length'),
+				getStoryData('Words'),
 				getStoryData('Type'),
 				getStoryData('Complete'),
 				getStoryData('Setting'),
@@ -61,12 +75,36 @@ function loadTableData(JsonData){
 				getStoryData('Anna'),
 				getStoryData('Powers'),
 				getStoryData('Sisters'),
-				getStoryData('Rating'),
+				getStoryData('Rated'),
 				getStoryData('Smut'),
 				getStoryData('Added'),
 				getStoryData('Published')
 			);
 	}
-	var table = document.getElementById('content-table');
-	table.innerHTML = result;
+	var content = document.getElementById('table-content');
+	content.innerHTML = result;
 }
+
+function DataObjCmp(key,revert){
+	return function(a,b){
+		if(a[key] < b[key])
+			return revert?  1 : -1;
+		if(a[key] > b[key])
+			return revert? -1 :  1;
+		return 0;
+	}
+}
+
+var justSort = 'Title';
+
+$(document).ready(function(){
+	$('.sortable').click(function(){
+		console.log('sort',this.innerHTML);
+		loadTableData(DataObj.sort(DataObjCmp(this.innerHTML,justSort==this.innerHTML)));
+		justSort = justSort==this.innerHTML?'':this.innerHTML;
+		$('.sortable').removeAttr('style');
+		$(this).attr('style','background-color: #0099ee;');
+	});
+});
+
+
